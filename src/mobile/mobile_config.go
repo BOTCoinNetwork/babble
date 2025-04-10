8585e135fe91afd52ba108bd702a17f0aa0c2cf2
stackmanxxx <stackmanxxx@gmail.com>
2025-04-10 10:31:41 +0800

    fix: go mod & v0.6.0

---
+++ src/mobile/mobile_config.go
@@ -0,0 +1,88 @@
+package mobile
+
+import (
+	"time"
+
+	"github.com/BOTCoinNetwork/babble/src/config"
+)
+
+// MobileConfig ...
+type MobileConfig struct {
+	Heartbeat      int    //heartbeat timeout in milliseconds
+	SlowHeartbeat  int    //slow-heartbeat in milliseconds
+	TCPTimeout     int    //TCP timeout in milliseconds
+	MaxPool        int    //Max number of pooled connections
+	CacheSize      int    //Number of items in LRU cache
+	SyncLimit      int    //Max Events per sync
+	EnableFastSync bool   //Enable fast sync
+	Store          bool   //Use badger store
+	LogLevel       string //debug, info, warn, error, fatal, panic
+	Moniker        string //optional name
+	SuspendLimit   int    //number of undetermined-events before suspension
+}
+
+// NewMobileConfig ...
+func NewMobileConfig(heartbeat int,
+	slowHeartbeat int,
+	tcpTimeout int,
+	maxPool int,
+	cacheSize int,
+	syncLimit int,
+	enableFastSync bool,
+	store bool,
+	logLevel string,
+	moniker string,
+	suspendLimit int) *MobileConfig {
+
+	return &MobileConfig{
+		Heartbeat:      heartbeat,
+		SlowHeartbeat:  slowHeartbeat,
+		MaxPool:        maxPool,
+		TCPTimeout:     tcpTimeout,
+		CacheSize:      cacheSize,
+		SyncLimit:      syncLimit,
+		EnableFastSync: enableFastSync,
+		Store:          store,
+		LogLevel:       logLevel,
+		Moniker:        moniker,
+		SuspendLimit:   suspendLimit,
+	}
+}
+
+// DefaultMobileConfig ...
+func DefaultMobileConfig() *MobileConfig {
+	return &MobileConfig{
+		Heartbeat:      20,
+		SlowHeartbeat:  200,
+		TCPTimeout:     200,
+		MaxPool:        2,
+		CacheSize:      500,
+		SyncLimit:      1000,
+		EnableFastSync: true,
+		Store:          false,
+		LogLevel:       "debug",
+		SuspendLimit:   1000,
+	}
+}
+
+// toBabbleConfig converts a MobileConfig to a BabbleConfig forcing the service-
+// related options to disable the API service.
+func (c *MobileConfig) toBabbleConfig() *config.Config {
+	babbleConfig := config.NewDefaultConfig()
+
+	babbleConfig.MaxPool = c.MaxPool
+	babbleConfig.Store = c.Store
+	babbleConfig.LogLevel = c.LogLevel
+	babbleConfig.Moniker = c.Moniker
+	babbleConfig.HeartbeatTimeout = time.Duration(c.Heartbeat) * time.Millisecond
+	babbleConfig.SlowHeartbeatTimeout = time.Duration(c.SlowHeartbeat) * time.Millisecond
+	babbleConfig.TCPTimeout = time.Duration(c.TCPTimeout) * time.Millisecond
+	babbleConfig.CacheSize = c.CacheSize
+	babbleConfig.SyncLimit = c.SyncLimit
+	babbleConfig.EnableFastSync = c.EnableFastSync
+	babbleConfig.SuspendLimit = c.SuspendLimit
+	babbleConfig.ServiceAddr = ""
+	babbleConfig.NoService = true
+
+	return babbleConfig
+}
---
+++ src/proxy/dummy/state.go
@@ -0,0 +1,112 @@
+package dummy
+
+import (
+	"fmt"
+
+	"github.com/BOTCoinNetwork/babble/src/crypto"
+	"github.com/BOTCoinNetwork/babble/src/hashgraph"
+	"github.com/BOTCoinNetwork/babble/src/proxy"
+	"github.com/sirupsen/logrus"
+)
+
+/*
+* The dummy App is used for testing and as an example for building Babble
+* applications. Here, we define the dummy's state which doesn't really do
+* anything useful. It saves and logs block transactions. The state hash is
+* computed by cumulatively hashing transactions together as they come in.
+* Snapshots correspond to the state hash resulting from executing a the block's
+* transactions.
+ */
+
+// State ...
+type State struct {
+	committedTxs [][]byte
+	stateHash    []byte
+	snapshots    map[int][]byte
+	logger       *logrus.Entry
+}
+
+// NewState ...
+func NewState(logger *logrus.Entry) *State {
+	state := &State{
+		committedTxs: [][]byte{},
+		stateHash:    []byte{},
+		snapshots:    make(map[int][]byte),
+		logger:       logger,
+	}
+
+	logger.Info("Init Dummy State")
+
+	return state
+}
+
+// CommitHandler ...
+func (a *State) CommitHandler(block hashgraph.Block) (proxy.CommitResponse, error) {
+
+	if a.logger.Level > logrus.InfoLevel {
+		blockBytes, _ := block.Marshal()
+		a.logger.WithField("block", string(blockBytes)).Debug("CommitBlock")
+	}
+
+	err := a.commit(block)
+	if err != nil {
+		return proxy.CommitResponse{}, err
+	}
+
+	receipts := []hashgraph.InternalTransactionReceipt{}
+	for _, it := range block.InternalTransactions() {
+		r := it.AsAccepted()
+		receipts = append(receipts, r)
+	}
+
+	response := proxy.CommitResponse{
+		StateHash:                   a.stateHash,
+		InternalTransactionReceipts: receipts,
+	}
+
+	return response, nil
+}
+
+// SnapshotHandler ...
+func (a *State) SnapshotHandler(blockIndex int) ([]byte, error) {
+	a.logger.WithField("block", blockIndex).Debug("GetSnapshot")
+
+	snapshot, ok := a.snapshots[blockIndex]
+
+	if !ok {
+		return nil, fmt.Errorf("Snapshot %d not found", blockIndex)
+	}
+
+	return snapshot, nil
+}
+
+// RestoreHandler ...
+func (a *State) RestoreHandler(snapshot []byte) ([]byte, error) {
+	a.stateHash = snapshot
+
+	return a.stateHash, nil
+}
+
+// GetCommittedTransactions ...
+func (a *State) GetCommittedTransactions() [][]byte {
+	return a.committedTxs
+}
+
+func (a *State) commit(block hashgraph.Block) error {
+	a.committedTxs = append(a.committedTxs, block.Transactions()...)
+
+	//log tx and update state hash
+	hash := a.stateHash
+
+	for _, tx := range block.Transactions() {
+		a.logger.Info(string(tx))
+
+		hash = crypto.SimpleHashFromTwoHashes(hash, crypto.SHA256(tx))
+	}
+
+	a.stateHash = hash
+
+	a.snapshots[block.Index()] = hash
+
+	return nil
+}
